d185ca91700f53611050d2bad68fb05542b3d705
HydraPV <160309620+hydraPV@users.noreply.github.com>
2025-05-28 23:22:49 -0400

    [Everforest] Synced Configs with Gruvbox

--- Everforest/.config/nvim/init.lua
+++ Everforest/.config/nvim/init.lua
@@ -20,9 +20,9 @@ vim.opt.cursorline = true
 vim.opt.fillchars = {eob = ' '}
 
 -- Set <tab> space
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 
 -------------------------------------------------
