040d438ee705ddbf3fb98326d92544fb669654f3
HydraPV <160309620+hydraPV@users.noreply.github.com>
2025-06-04 21:28:07 -0400

    [Neovim] Re formated Telescope and deleted Telescope-MF

--- Gruvbox/.config/nvim/lua/plugins/Telescope-MF.lua
+++
@@ -1,6 +0,0 @@
-return {
-    'nvim-telescope/telescope-media-files.nvim',
-    dependencies = {'nvim-lua/popup.nvim'},
-
-    config = function() require('telescope').load_extension('media_files') end
-}
--- Gruvbox/.config/nvim/lua/plugins/telescope.lua
+++ Gruvbox/.config/nvim/lua/plugins/telescope.lua
@@ -1,28 +1,22 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    event = "VimEnter",
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'}
-    },
-    config = function()
-        local telescope = require('telescope')
-
-        telescope.setup({
-            extensions = {
-                media_files = {
-                    filetypes = {"png", "webp", "jpg", "jpeg"},
-                    find_cmd = "rg"
-                },
-                fzf = {
-                    fuzzy = true,
-                    override_generic_sorter = true,
-                    override_file_sorter = true
-                }
-            }
-        })
-
-        telescope.load_extension('fzf')
-    end
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {'nvim-lua/plenary.nvim',{'nvim-telescope/telescope-fzf-native.nvim', build = 'make'}},
+  config = function()
+    local telescope = require('telescope')
+    telescope.setup({
+      extensions = {
+        media_files = {
+          filetypes = {"png", "webp", "jpg", "jpeg"},
+          find_cmd = "rg"
+        },
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true
+        }
+      }
+    })
+    telescope.load_extension('fzf')
+  end
 }
-
