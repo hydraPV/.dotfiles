938d4e0fc0117532e37f3fdfa72eb732f64d365a
HydraPV <160309620+hydraPV@users.noreply.github.com>
2026-02-17 20:36:16 -0300

    refactor(Riceswap): Removed comments and echo's

--- RiceSwap.sh
+++ RiceSwap.sh
@@ -23,74 +23,58 @@ cat << ASCII
 
 ASCII
 
-# Reload function
 Reload() {
-    hyprctl reload &>/dev/null && 
-    killall waybar && waybar &>/dev/null &
-    killall hyprpaper && hyprpaper &>/dev/null &
-    killall mako && mako &>/dev/null &
+	hyprctl reload &>/dev/null &
+	killall waybar && waybar &>/dev/null &
+	killall hyprpaper && hyprpaper &>/dev/null &
+	killall mako && mako &>/dev/null & 
 }
 
-# General Symlink function
 Symlinks() {
-    SOURCE="$1"
-    REPEAT="$HOME/.config/kitty $HOME/.config/fish"
+	SOURCE="$1"
 
-    # Verifies the variable directories
-    echo " $SOURCE"
-    echo
+	printf "\n $SOURCE\n"
+	printf "󱄀 Creating Symlinks between $SOURCE and $HOME./.config\n"
 
-    sleep 0.5
+	ln -sf "$SOURCE"/* "$HOME/.config/"
 
-    # Create symlinks in base of the selected Rice
-    rm -rf "$REPEAT"
-    echo "󱄀 Creating Symlinks between $SOURCE and $HOME./.config"
-    ln -sf "$SOURCE"/* "$HOME/.config/"
-    echo
+	printf "\n󱉶 Verifying symlinks in $HOME./.config\n"
+	for file in "$SOURCE"/*; do
+		basefile=$(basename "$file")
+		if [ -L "$HOME/.config/$basefile" ]; then
+			printf " $basefile\n"
+		else
+			printf " $basefile Please do this symlink manually later\n"
+		fi
+	done
 
-    sleep 0.5
+	sleep 1
 
-    # Checks if symlinks were correctly created
-    echo "󱉶 Verifying symlinks in $HOME./.config"
-    for file in "$SOURCE"/*; do
-        basefile=$(basename "$file")
-        if [ -L "$HOME/.config/$basefile" ]; then
-            echo " $basefile"
-        else
-            echo " $basefile Please do this symlink manually later"
-        fi
-    done
-    echo
-
-    sleep 1
-
-    echo "  Swap complete."
+	printf "\n Swap complete.\n"
 }
 
-
-echo "󰟪 Select a Rice"
+printf "󰟪 Select a Rice\n"
 while true; do
-    echo -e "\033[1mGruvbox\033[0m [G] / \033[1mEverforest\033[0m [E]"
-    read -r -p "󱞩 " Prompt
-    Prompt="${Prompt,,}"
-    [[ "$Prompt" =~ ^(g|e|)$ ]] && break
-    echo " Please enter [g/e]."
+	printf "\033[1mGruvbox\033[0m [G] / \033[1mEverforest\033[0m [E]\n"
+	read -r -p "󱞩 " Prompt
+	Prompt="${Prompt,,}"
+	[[ "$Prompt" =~ ^(g|e|)$ ]] && break
+	printf " Please enter [g/e].\n"
 done
 
 case "$Prompt" in
-    g)
-        Symlinks "$HOME/.dotfiles/Gruvbox/.config/"
-        ln -sf "/usr/share/themes/Gruvbox/gtk-4.0/" "$HOME/.dotfiles/GTK/"
-        Reload
-        sleep 3 && exit ;;
-    e)
-        Symlinks "$HOME/.dotfiles/Everforest/.config/"
-        ln -sf "/usr/share/themes/Everforest/gtk-4.0/" "$HOME/.dotfiles/GTK/"
-        Reload
-        sleep 3 && exit ;;
-    *)
-        echo "Aborting..."
-        sleep 3
-        exit 1
-        ;;
+	g)
+		Symlinks "$HOME/.dotfiles/Gruvbox/.config"
+		Reload
+		exit 0
+		;;
+	e)
+		Symlinks "$HOME/.dotfiles/Everforest/.config"
+		Reload
+		exit 0
+		;;
+	*)
+		printf "Aborting..."
+		exit 1
+		;;
 esac
